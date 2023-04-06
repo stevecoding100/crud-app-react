c2c42b53183a21385d37c2e0bfe97a3f8fd12021
Steve De La Rosa <stevedelarosa1036@gmail.com>
2023-04-06 15:13:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
-
+import UserTable from "./tables/UserTable";
+import AddUserForm from "./tables/AddUserForm";
+import EditUserForm from "./form/EditUserForm";
+import { useState } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Dummy User Data
+
+    const usersData = [
+        { id: 1, name: "Tania", username: "floppydiskette" },
+        { id: 2, name: "Craig", username: "siliconeidolon" },
+        { id: 3, name: "Ben", username: "benisphere" },
+    ];
+
+    const initialFormState = { id: null, name: "", username: "" };
+
+    const [users, setUsers] = useState(usersData);
+
+    const [editing, setEditing] = useState(false);
+
+    const [currentUser, setCurrentUser] = useState(initialFormState);
+
+    // Since we don't know who is being edited until it's selected, we'll create initial empty state for the form, like we did with the add form.
+
+    const addUser = (user) => {
+        user.id = users.length + 1;
+        setUsers([...users, user]);
+    };
+
+    const deleteUser = (id) => {
+        setUsers(users.filter((user) => user.id !== id));
+        setEditing(false);
+    };
+
+    const editRow = (user) => {
+        setEditing(true);
+
+        setCurrentUser({
+            id: user.id,
+            name: user.name,
+            username: user.username,
+        });
+    };
+
+    const updateUser = (id, updatedUser) => {
+        setEditing(false);
+
+        setUsers(users.map((user) => (user.id === id ? updatedUser : user)));
+    };
+
+    return (
+        <>
+            <div className="container">
+                <h1>CRUD App with Hooks</h1>
+                <div className="flex-row">
+                    <div className="flex-large">
+                        {editing ? (
+                            <div>
+                                <h2>Edit user</h2>
+                                <EditUserForm
+                                    setEditing={setEditing}
+                                    currentUser={currentUser}
+                                    updateUser={updateUser}
+                                />
+                            </div>
+                        ) : (
+                            <div>
+                                <h2>Add user</h2>
+                                <AddUserForm addUser={addUser} />
+                            </div>
+                        )}
+                    </div>
+                    <div className="flex-large">
+                        <h2>View users</h2>
+                        <UserTable
+                            users={users}
+                            deleteUser={deleteUser}
+                            editRow={editRow}
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/tables/UserTable.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "../index.css";
+const UserTable = (props) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {/* Using the ternary condition ... if users data is > 0 then display some users else display <tr> <td> No users</td> </tr> */}
+                {props.users.length > 0 ? (
+                    props.users.map((user) => (
+                        <tr key={user.id}>
+                            <td>{user.name}</td>
+                            <td>{user.username}</td>
+                            <td>
+                                <button
+                                    className="button muted-button"
+                                    onClick={() => {
+                                        props.editRow(user);
+                                    }}
+                                >
+                                    Edit
+                                </button>
+                                <button
+                                    className="button muted-button"
+                                    onClick={() => props.deleteUser(user.id)}
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))
+                ) : (
+                    <tr>
+                        <td colSpan={3}>No Users</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    );
+};
+
+export default UserTable;
